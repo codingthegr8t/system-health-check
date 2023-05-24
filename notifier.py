9cf6f68889565f0b4143da8c85c0b29db37e1820
Chris <chrisschuilenburg@gmail.com>
2023-05-25 01:13:20 +0200

    Modified the exception for network more specific

--- notifier.py
+++ notifier.py
@@ -168,7 +168,7 @@ class Notifier:
         try:
             socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
             return True
-        except Exception as err:
+        except socket.error as err:
             logging.error("Network connection unavailable. %s", err)
             return False
 
